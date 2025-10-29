66295ec6ec4ca053f66e62adf0bbd6ec83983987
rdlm24 <roberto.deleonmartinez@gmail.com>
2025-10-29 04:50:26 +0000

    Deploying to gh-pages from @ rdlm24/rdlm24.github.io@4d782da99718ad2698a23693bcd97dd6465261e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-acerca-de",
-    title: "Acerca de",
+    id: "nav-inicio",
+    title: "Inicio",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
