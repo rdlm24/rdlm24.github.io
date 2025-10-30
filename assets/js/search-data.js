193ffa81f6d60c48f9c33ccd695308d32f7c13c2
rdlm24 <roberto.deleonmartinez@gmail.com>
2025-10-30 01:49:02 +0000

    Deploying to gh-pages from @ rdlm24/rdlm24.github.io@ea7382457cea589fc60ec0dff8a133137dfa242c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -160,6 +160,17 @@ ninja.data = [{
             window.location.href = "/blog/2024/code-diff/";
           
         },
+      },{id: "post-a-post-with-advanced-image-components",
+        
+          title: "a post with advanced image components",
+        
+        description: "this is what advanced image components could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/advanced-images/";
+          
+        },
       },{id: "post-a-post-with-vega-lite",
         
           title: "a post with vega lite",
